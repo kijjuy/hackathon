099dc7ed8c5658178c641af344381f6957d2aca9
Brandon <brandonzomer@gmail.com>
2023-12-02 04:36:16 -0500

    Got appUser working, and redirect to page if desc is empty

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using hackathon.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Runtime.CompilerServices;
+using hackathon.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace hackathon.Controllers;
 
@@ -8,20 +12,36 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(
+        ILogger<HomeController> logger,
+        ApplicationDbContext context,
+        UserManager<ApplicationUser> userManager
+        )
     {
         _logger = logger;
+        _context = context;
+        _userManager = userManager;
     }
 
-    public IActionResult Index()
+    [Authorize]
+    public async Task<IActionResult> Index()
     {
+        var user = await _userManager.GetUserAsync(User);
+        if(user.Description != null && user.Description != "") {
+            Console.WriteLine($"{user.Description}");
+            return View();
+        }
+        return View("CreateProfile");
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> UpdateDescription() {
         
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 public class ApplicationUser: IdentityUser {
@@ -5,7 +6,11 @@ public class ApplicationUser: IdentityUser {
     //TODO verify that email is mohawk college email
     //TODO add any other things for user
 
+    [Required]
+    [StringLength(500, MinimumLength = 150)]
     public string? Description { get; set; }   
+
+
 
     public List<Tag> Tags { get; set; }
 
