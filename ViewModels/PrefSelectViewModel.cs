ef11c813c54bf6d3b59bceb4099017d5160918e5
Brandon <brandonzomer@gmail.com>
2023-12-02 14:08:53 -0500

    got it to work

--- ViewModels/PrefSelectViewModel.cs
+++ ViewModels/PrefSelectViewModel.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace hackathon.ViewModels;
 
 public class PrefSelectViewModel {
+
+    [Display(Name = "Find a housemate")]
     public bool HousePref { get; set; }
+    
+    [Display(Name = "Find a studymate")]
     public bool StudyPref { get; set; }
+
+    [Display(Name = "Find a friend")]
     public bool FriendPref { get; set; }
 
 }
